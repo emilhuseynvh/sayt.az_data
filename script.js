7b3e3446bab73d71ed9f1b720598c8e2d9513b35
emilhuseynvh <emilhuseynvh@gmail.com>
2024-05-26 17:07:04 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,206 @@
+let data = [
+    {
+        "id": 5,
+        "name": "Səhifə",
+        "price": 100,
+        "type": "static_page"
+    },
+    {
+        "id": 10,
+        "name": "Orta",
+        "price": 2,
+        "type": "design_complexity"
+    },
+    {
+        "id": 12,
+        "name": "Sadə",
+        "price": 1,
+        "type": "design_complexity"
+    },
+    {
+        "id": 13,
+        "name": "Xüsusi",
+        "price": 3,
+        "type": "design_complexity"
+    },
+    {
+        "id": 7,
+        "name": "Xüsusi",
+        "price": 200,
+        "type": "logo_design"
+    },
+    {
+        "id": 15,
+        "name": "Ehtiyac yoxdur",
+        "price": 0,
+        "type": "logo_design"
+    },
+    {
+        "id": 20,
+        "name": "Sadə",
+        "price": 100,
+        "type": "logo_design"
+    },
+    {
+        "id": 1,
+        "name": "Bloq",
+        "price": 200,
+        "type": "website_addons"
+    },
+    {
+        "id": 2,
+        "name": "Üzv girişi / Profillər",
+        "price": 400,
+        "type": "website_addons"
+    },
+    {
+        "id": 8,
+        "name": "Responsive",
+        "price": 100,
+        "type": "website_addons"
+    },
+    {
+        "id": 22,
+        "name": "Forum",
+        "price": 400,
+        "type": "website_addons"
+    },
+    {
+        "id": 23,
+        "name": "Mesajlaşma",
+        "price": 50,
+        "type": "website_addons"
+    },
+    {
+        "id": 24,
+        "name": "Canlı Söhbət",
+        "price": 50,
+        "type": "website_addons"
+    },
+    {
+        "id": 25,
+        "name": "Qalereya",
+        "price": 100,
+        "type": "website_addons"
+    },
+    {
+        "id": 26,
+        "name": "Əlaqə Forması",
+        "price": 100,
+        "type": "website_addons"
+    },
+    {
+        "id": 27,
+        "name": "Saytda Axtarış",
+        "price": 100,
+        "type": "website_addons"
+    },
+    {
+        "id": 28,
+        "name": "Xəritə Məlumatı / Geolocation",
+        "price": 100,
+        "type": "website_addons"
+    },
+    {
+        "id": 29,
+        "name": "Rezervasyon",
+        "price": 400,
+        "type": "website_addons"
+    },
+    {
+        "id": 30,
+        "name": "Sosial Media inteqrasiyası",
+        "price": 50,
+        "type": "website_addons"
+    },
+    {
+        "id": 31,
+        "name": "Analitika / İzləmə",
+        "price": 50,
+        "type": "website_addons"
+    },
+    {
+        "id": 36,
+        "name": "Admin Panel",
+        "price": 300,
+        "type": "website_addons"
+    },
+    {
+        "id": 3,
+        "name": "Başlıqlar & Meta",
+        "price": 200,
+        "type": "seo_addons"
+    },
+    {
+        "id": 32,
+        "name": "Açar söz",
+        "price": 300,
+        "type": "seo_addons"
+    },
+    {
+        "id": 33,
+        "name": "+20 Qabaqcıl Texnika",
+        "price": 300,
+        "type": "seo_addons"
+    },
+    {
+        "id": 34,
+        "name": "Başlıq Teqləri",
+        "price": 100,
+        "type": "seo_addons"
+    },
+    {
+        "id": 35,
+        "name": "Sitemap",
+        "price": 100,
+        "type": "seo_addons"
+    },
+    {
+        "id": 18,
+        "name": "min",
+        "price": 450,
+        "type": "setting"
+    },
+    {
+        "id": 19,
+        "name": "page",
+        "price": 20,
+        "type": "setting"
+    },
+    {
+        "id": 38,
+        "name": "Ödəniş",
+        "price": 200,
+        "type": "ecommerce"
+    },
+    {
+        "id": 39,
+        "name": "Səbət",
+        "price": 500,
+        "type": "ecommerce"
+    },
+    {
+        "id": 40,
+        "name": "Məhsullar",
+        "price": 300,
+        "type": "ecommerce"
+    },
+    {
+        "id": 41,
+        "name": "Bəyəndiklərim",
+        "price": 300,
+        "type": "ecommerce"
+    },
+    {
+        "id": 42,
+        "name": "Filtr",
+        "price": 300,
+        "type": "ecommerce"
+    },
+    {
+        "id": 43,
+        "name": "Məhsul Haqqında",
+        "price": 200,
+        "type": "ecommerce"
+    }
+];
